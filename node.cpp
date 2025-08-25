21a91fe016a16a6bbe66d26f15065f3804ab5998
Kirill Artamonov (office) <artamonovkg@gmail.com>
2025-08-25 15:44:14 +0300

    First ccommit

---
+++ node.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+
+
+int main(int argc, char* argv[])
+{
+	printf("I am node!\n");
+	return 0;
+}
